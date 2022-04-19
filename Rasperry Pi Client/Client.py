40d6bce5f817d2d526aec3cf1509daaf240f9ef8
Nikhil Jeikar <59568596+NikhilJeikar@users.noreply.github.com>
2022-04-19 11:42:47 +0530

    Added Analytics Part

--- Rasperry Pi Client/Client.py
+++ Rasperry Pi Client/Client.py
@@ -1,7 +1,5 @@
 from socket import *
 import json
-
-import numpy as np
 from picamera import PiCamera
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -48,58 +46,34 @@ Mode1 = "Mode1"
 Mode2 = "Mode2"
 
 AnalyticsMode = True
-CameraUsage = False
 
 # Main part
+while True:
+    try:
+        Main.connect((IP, Port))
+        break
+    except Exception:
+        pass
 
-Main.connect((IP, Port))
 camera = PiCamera()
 
-camera.resolution = (250, 250)
-camera.framerate = 30
-
-
-class CameraOut:
-    def __init__(self):
-        self.size = 0
-
-    def write(self, s):
-        self.size += len(s)
-        print(s)
-
-    def flush(self):
-        print('%d bytes would have been written' % self.size)
+camera.resolution = (200, 200)
+camera.vflip = True
+camera.contrast = 10
 
 
 def Parser(String):
     return f"{len(String)}{Split}{String}".encode()
 
 
-CameraObj = CameraOut()
-
-
-def InitCamera():
-    global CameraUsage
-    if not CameraUsage:
-        camera.start_recording(CameraObj)
-        CameraUsage = True
-
-
-def CloseCamera():
-    global CameraUsage
-    if CameraUsage:
-        camera.stop_recording()
-        CameraUsage = False
-
-
 while True:
     if AnalyticsMode:
-        InitCamera()
-        Img = np.zeros((100, 100, 3))
+        file_name = "/home/pi/Pictures/img_tmp" + ".jpg"
+        camera.capture(file_name)
+        Img = plt.imread(file_name)
         Time = datetime.now()
         Main.send(Parser(Data(Img.tolist(), "", Time.year, Time.month, Time.day, Time.hour, Time.minute)))
     else:
-        CloseCamera()
         Main.send(Data("", "", 0, 0, 0, 0, 0))
         "Motion Detection part here"
     Command = Read(Main, Buffer)
